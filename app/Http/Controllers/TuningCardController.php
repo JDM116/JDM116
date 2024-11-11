8ff8c3f6ce16f2d4c3a749b53a7ebddb59ef52e8
Dmitry Komlev <zanozared228@gmail.com>
2024-11-11 14:54:47 +0300

    cards

--- app/Http/Controllers/TuningCardController.php
+++ app/Http/Controllers/TuningCardController.php
@@ -8,18 +8,18 @@ use Illuminate\Http\Request;
 class TuningCardController extends Controller
 {
     public function index(){
-        TuningCardModel::orderBy("created_at","desc")->get();
-        return view("welcome");
+        $tuningcards = TuningCardModel::orderBy("created_at","desc")->get();
+        return view("welcome", compact('tuningcards'));
     }
     public function create(){
-  TuningCardModel::new([
-    'title'=> request('title') ,
-    'image'=> request('image') ,
-    'description'=> request('description') ,
-    'amount'=> request('amount') ,
-    'cost'=> request('cost') ,
-  ]);
-  return redirect('/');
+        TuningCardModel::new([
+            'title'=> request('title') ,
+            'image'=> request('image') ,
+            'description'=> request('description') ,
+            'amount'=> request('amount') ,
+            'cost'=> request('cost') ,
+        ]);
+        return redirect('/');
 
     }
 }
